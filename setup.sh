218edee1c516f8527af0a109c8cbf988f09b1ec8
Ani Aggarwal <antiani100@gmail.com>
2022-08-21 17:17:55 -0700

    Add need to make conda env to setup.sh

--- setup.sh
+++ setup.sh
@@ -33,7 +33,6 @@ setup_nvim() {
 
     # ln -s "$SCRIPT_DIR/.config/nvim/" "$HOME/.config/nvim"
 
-    # sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     # Create conda env
     # ln ~/.config/nvim or whatever
 
