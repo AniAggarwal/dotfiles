6f124eba665f1442063df2519c304e6ab65d7a94
Ani Aggarwal <antiani100@gmail.com>
2022-09-07 12:13:01 -0400

    Add checking for eclipse projects to project.nvim

--- .config/nvim/lua/user/project.lua
+++ .config/nvim/lua/user/project.lua
@@ -3,5 +3,6 @@ if not status_ok then
 	return
 end
 project.setup({
-
+    -- add .project and .classpath to defaults for Eclipse projects
+	patterns = { ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", ".project", ".classpath" },
 })
