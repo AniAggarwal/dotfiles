2ae9a38cae559f1cca04678282b6734f4edeace7
Ani Aggarwal <antiani100@gmail.com>
2022-09-07 21:48:36 -0400

    Remove icons form todo comment.

--- .config/nvim/lua/user/todo_comments.lua
+++ .config/nvim/lua/user/todo_comments.lua
@@ -1,1 +1,14 @@
-require("todo-comments").setup({})
+require("todo-comments").setup({
+	keywords = {
+		-- Turning off icons so that gitsigns aren't interrupted
+		FIX = {
+			icon = "",
+		},
+		TODO = { icon = "" },
+		HACK = { icon = "" },
+		WARN = { icon = "" },
+		PERF = { icon = "" },
+		NOTE = { icon = "" },
+		TEST = { icon = "" },
+	},
+})
