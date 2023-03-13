6a966cfd918069f0fa960cda45ec9183647936f8
Ani Aggarwal <antiani100@gmail.com>
2023-03-13 16:28:21 -0400

    Add scripts for brightness. Broken atm

--- decrease-brightness.sh
+++ decrease-brightness.sh
@@ -1,3 +1,4 @@
+sudo chmod 646 /sys/class/backlight/intel_backlight/brightness
 # If brightness is greater than 4, set it to 0
 # Else, add 1 to the current brightness
 current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
