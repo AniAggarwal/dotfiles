e0e38837e29035eb8b7c3684cac3f932389fdd57
Ani Aggarwal <antiani100@gmail.com>
2023-07-13 13:57:37 -0700

    Update Hyprland config and add it to script.

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -12,7 +12,7 @@ usage() {
 }
 
 install_all_no_sudo() {
-    stow -d "$dir" -t ~/ zsh kitty nvim bat conky easyeffects ignores bin
+    stow -d "$dir" -t ~/ zsh kitty nvim bat conky easyeffects ignores bin hyprland
 }
 
 install_all_sudo() {
