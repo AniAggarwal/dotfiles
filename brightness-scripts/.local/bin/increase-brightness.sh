09d36ac7a3fc8f4ffffde390f924bc4a66642ebe
Ani Aggarwal <antiani100@gmail.com>
2023-10-01 14:26:03 -0400

    Remove old scripts

--- brightness-scripts/.local/bin/increase-brightness.sh
+++
@@ -1,6 +0,0 @@
-sudo chmod 646 /sys/class/backlight/intel_backlight/brightness
-# If brightness is greater than 4, set it to 0
-# Else, add 1 to the current brightness
-current_brightness=$(cat /sys/class/backlight/intel_backlight/brightness)
-
-echo $((current_brightness + 1)) | tee /sys/class/backlight/intel_backlight/brightness
